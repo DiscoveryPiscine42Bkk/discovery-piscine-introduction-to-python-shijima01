f186f3a78c3c7eed2cd1cb803502209a00de3068
Steve <119849991+shijima01@users.noreply.github.com>
2025-06-26 00:59:49 +0000

    add to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number that less than 25\n"))
+if number <= 25:
+    while number <= 25:
+        print("Number is",number)
+        number +=1
+else:
+    print("Error")
+
+
